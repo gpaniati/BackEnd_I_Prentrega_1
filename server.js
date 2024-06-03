308207934ceadf19a1d9f0d9f4a1401ec17727d3
gpaniati <gpaniati@gmail.com>
2024-06-02 22:25:17 -0300

    server version 1.1

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ server.use(express.json());
 const products = new ProductManager();
 //const carts = new CartManager();
 
-
+// ENDPOINTS DE PRODUCTOS.
+// Endpoint: Método GET que escucha en la URL http://localhost:8080/api/productos/
+// Listar todos los productos de la base.
 server.get('/api/products/', async (req, res) => {
     const productos = await products.consultarProductos();
+
     if (!productos) {
         return res.status(400).send({ status: "error", message: "No hay ningún producto cargado" });
     }
@@ -22,6 +25,19 @@ server.get('/api/products/', async (req, res) => {
     return res.status(200).send({ status: "success", payload: productos });
 });
 
+// Endpoint: Método GET que escucha en la URL http://localhost:8080/api/productos/:pid
+// Obtiene sólo el producto con el id proporcionado.
+server.get('/api/products/:pid', async (req, res) => {
+    const { pid } = req.params;
+    const productos = await products.consultarProductos();
+    const producto = productos.find((producto) => producto.id === Number(pid));
+
+    if (!producto) {
+        return res.status(400).send({ status: "error", message: "Producto no encontrado" });
+    }
+
+    return res.status(200).send({ status: "success", payload: producto });
+});
 
 // Método que responde a las URL inexistentes
 server.use("*", (req, res) => {
